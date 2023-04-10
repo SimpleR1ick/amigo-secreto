d6fc1d2df4a3c1c278c2a7535f2eb1b7f810ac76
Henrique Dalmagro <84425302+SimpleR1ick@users.noreply.github.com>
2023-04-10 14:14:32 -0300

    Project template 5

--- src/config/server.js
+++ src/config/server.js
@@ -4,18 +4,24 @@ const bodyParser = require('body-parser');
 
 const path = require("path");
 
+// Paths
+var views  = path.join(__dirname, '../views')
+var layout = path.join(__dirname, '../views/pages/layouts/layout')
+var public = path.join(__dirname, '../public')
+
+// Carregando variaveis de ambiente
 require("dotenv").config();
 
 const app = express();
 
-app.use(expressLayouts);
-
 // View engine
 app.set('view engine', 'ejs');
-app.set('views',  path.join(__dirname, '../views'));
-app.set('layout', path.join(__dirname, '../views/pages/layouts/layout'));
+app.set('views', views);
+app.set('layout', layout);
 
-app.use(bodyParser.urlencoded({extended: false}));
-app.use(express.static(path.join(__dirname, '../public')));
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.use(express.static(public));
+app.use(expressLayouts);
 
 module.exports = app;
